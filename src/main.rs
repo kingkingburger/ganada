b19b985c052f5149de4bbc821aaa3e1b43d4b0b0
minho <dnjsalsgh123@gmail.com>
2025-07-22 09:39:44 +0900

    update: delete unused main

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use ganada::{is_korean, decompose};
-
-fn main() {
-    let text = "안녕 Rust!";
-    println!("is_korean: {}", is_korean(text));
-    println!("decompose: {:?}", decompose(text));
-    
-    println!("compose: {}", compose(decompose(text)));
-}
